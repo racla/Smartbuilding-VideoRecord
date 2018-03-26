35ed52566c6f38ba53c83e1db419344ec1ac216d
wanyangyang <wyy0704@qq.com>
2018-03-26 20:24:18 +0800

    在139.199.210.152服务器上拉流存储萤石云视频程序
    ！此版本由于缺少获得设备状态的接口不能运行

---
+++ ffmpegForRTMP/HTMLHelper.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ffmpegForRTMP
+{
+    public class HTMLHelper
+    {
+        /// <summary>
+        /// 获取CooKie
+        /// </summary>
+        /// <param name="loginUrl"></param>
+        /// <param name="postdata"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
+        public static CookieContainer GetCooKie(string loginUrl, string postdata, HttpHeader header)
+        {
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
+            try
+            {
+                CookieContainer cc = new CookieContainer();
+                request = (HttpWebRequest)WebRequest.Create(loginUrl);
+                request.Method = header.method;
+                request.ContentType = header.contentType;
+                byte[] postdatabyte = Encoding.UTF8.GetBytes(postdata);     //提交的请求主体的内容
+                request.ContentLength = postdatabyte.Length;    //提交的请求主体的长度
+                request.AllowAutoRedirect = false;
+                request.CookieContainer = cc;
+                request.KeepAlive = true;
+
+                //提交请求
+                Stream stream;
+                stream = request.GetRequestStream();
+                stream.Write(postdatabyte, 0, postdatabyte.Length);     //带上请求主体
+                stream.Close();
+
+                //接收响应
+                response = (HttpWebResponse)request.GetResponse();      //正式发起请求
+                response.Cookies = request.CookieContainer.GetCookies(request.RequestUri);
+
+                CookieCollection cook = response.Cookies;
+                //Cookie字符串格式
+                string strcrook = request.CookieContainer.GetCookieHeader(request.RequestUri);
+
+                return cc;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 获取html
+        /// </summary>
+        /// <param name="getUrl"></param>
+        /// <param name="cookieContainer"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
+        public static string GetHtml(string getUrl, CookieContainer cookieContainer, HttpHeader header)
+        {
+            Thread.Sleep(1000);
+            HttpWebRequest httpWebRequest = null;
+            HttpWebResponse httpWebResponse = null;
+            try
+            {
+                httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(getUrl);
+                httpWebRequest.CookieContainer = cookieContainer;
+                httpWebRequest.ContentType = header.contentType;
+                httpWebRequest.ServicePoint.ConnectionLimit = header.maxTry;
+                httpWebRequest.Referer = getUrl;
+                httpWebRequest.Accept = header.accept;
+                httpWebRequest.UserAgent = header.userAgent;
+                httpWebRequest.Method = "POST";
+                httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                Stream responseStream = httpWebResponse.GetResponseStream();
+                StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
+                string html = streamReader.ReadToEnd();
+                streamReader.Close();
+                responseStream.Close();
+                httpWebRequest.Abort();
+                httpWebResponse.Close();
+                return html;
+            }
+            catch (Exception e)
+            {
+                if (httpWebRequest != null) httpWebRequest.Abort();
+                if (httpWebResponse != null) httpWebResponse.Close();
+                return string.Empty;
+            }
+        }
+
+
+
+        /// <summary>
+        /// 获取html
+        /// </summary>
+        /// <param name="getUrl"></param>
+        /// <param name="cookieContainer"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
+        public static string POST(string url, CookieContainer cookieContainer, HttpHeader header, string data)
+        {
+            //Thread.Sleep(1000);
+            HttpWebRequest httpWebRequest = null;
+            HttpWebResponse httpWebResponse = null;
+            try
+            {
+                httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+                //httpWebRequest.ContentType = header.contentType;
+                //httpWebRequest.ServicePoint.ConnectionLimit = header.maxTry;
+                //httpWebRequest.Referer = url;
+                //httpWebRequest.Accept = header.accept;
+                //httpWebRequest.UserAgent = header.userAgent;
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+                //POST数据
+                byte[] postdatabyte = Encoding.UTF8.GetBytes(data);
+                httpWebRequest.ContentLength = postdatabyte.Length;
+                //httpWebRequest.AllowAutoRedirect = true; //是否跳转，需要设置，否则不执行php自动跳转看不到是否成功的页面
+                //httpWebRequest.CookieContainer = cookieContainer;
+                //httpWebRequest.KeepAlive = true;
+                httpWebRequest.ProtocolVersion = HttpVersion.Version11;
+                Stream stream;
+                stream = httpWebRequest.GetRequestStream();
+                stream.Write(postdatabyte, 0, postdatabyte.Length); //设置请求主体的内容
+                stream.Close();
+
+                httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                Stream responseStream = httpWebResponse.GetResponseStream();
+                StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
+                string html = streamReader.ReadToEnd();
+                streamReader.Close();
+                responseStream.Close();
+                httpWebRequest.Abort();
+                httpWebResponse.Close();
+                return html;
+            }
+            catch (Exception e)
+            {
+                if (httpWebRequest != null) httpWebRequest.Abort();
+                if (httpWebResponse != null) httpWebResponse.Close();
+                return string.Empty;
+            }
+        }
+
+        public static List<Cookie> GetAllCookies(CookieContainer cc0)
+        {
+            List<Cookie> lstCookies = new List<Cookie>();
+            Hashtable table = (Hashtable)cc0.GetType().InvokeMember("m_domainTable",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.GetField |
+                System.Reflection.BindingFlags.Instance, null, cc0, new object[] { });
+            foreach (object pathList in table.Values)
+            {
+                SortedList lstCookieCol = (SortedList)pathList.GetType().InvokeMember("m_list",
+                    System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.GetField
+                    | System.Reflection.BindingFlags.Instance, null, pathList, new object[] { });
+                foreach (CookieCollection colCookies in lstCookieCol.Values)
+                    foreach (Cookie c in colCookies) lstCookies.Add(c);
+            }
+            return lstCookies;
+        }
+        public static void SaveCookies(CookieContainer cc0)
+        {
+            StringBuilder sbc = new StringBuilder();
+            List<Cookie> cooklist = GetAllCookies(cc0);
+            foreach (Cookie cookie in cooklist)
+            {
+                sbc.AppendFormat("{0};{1};{2};{3};{4};{5}\r\n",
+                    cookie.Domain, cookie.Name, cookie.Path, cookie.Port,
+                    cookie.Secure.ToString(), cookie.Value);
+            }
+            FileStream fs = File.Create(@"chinarencookies.txt");
+            fs.Close();
+            File.WriteAllText(@"chinarencookies.txt", sbc.ToString(), System.Text.Encoding.Default);
+            string[] cookies = File.ReadAllText(@"chinarencookies.txt", System.Text.Encoding.Default).Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (string c in cookies)
+            {
+                string[] cc = c.Split(";".ToCharArray());
+                Cookie ck = new Cookie(); ;
+                ck.Discard = false;
+                ck.Domain = cc[0];
+                ck.Expired = true;
+                ck.HttpOnly = true;
+                ck.Name = cc[1];
+                ck.Path = cc[2];
+                ck.Port = cc[3];
+                ck.Secure = bool.Parse(cc[4]);
+                ck.Value = cc[5];
+                cc0.Add(ck);
+            }
+        }
+
+    }
+
+    public class HttpHeader
+    {
+        public string contentType { get; set; }
+
+        public string accept { get; set; }
+
+        public string userAgent { get; set; }
+
+        public string method { get; set; }
+
+        public int maxTry { get; set; }
+    }
+
+
+
+}
+
---
+++ ffmpegForRTMP/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ffmpegForRTMP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> old_sns = new List<string>();
+            List<string> old_rtmps = new List<string>();
+            while (true) {
+                List<string> new_sns = new List<string>();
+                List<string> new_rtmps = new List<string>();
+                using (SmartBuildingEntities sme = new SmartBuildingEntities())
+                {
+                    foreach (var item in sme.CamRTMP)
+                    {
+                        new_sns.Add(item.SN);
+                        new_rtmps.Add(item.RTMPhd);
+                    }
+                }
+                string[] sns = new_sns.Except(old_sns).ToArray();
+                string[] rtmps = new_rtmps.Except(old_rtmps).ToArray();
+                if (sns.Length > 0) {
+                    old_sns = new_sns;
+                    old_rtmps = new_rtmps;
+                    for (var i = 0; i < rtmps.Length; i++)
+                    {
+                        video temp = new video();
+                        temp.SN = sns[i];
+                        temp.RTMPhd = rtmps[i];
+                        Task.Factory.StartNew(SetTimer, temp);
+                        //SetTimer(temp);
+                    };
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.Read();
+        }
+        private class video {
+            public string SN;
+            public string RTMPhd;
+        }
+        private static void SetTimer(object video)
+        {
+            video temp = video as video;
+            string sn = temp.SN;
+            string RTMPhd = temp.RTMPhd;
+            bool isOnline = false;
+            //判断设备是否在线，在线的话执行一次SaveVideo,执行完了再判断是否在线
+            while(true){
+                while (!isOnline)
+                {
+                    string res = HTMLHelper.GetHtml("", null, null);
+                    System.Threading.Thread.Sleep(2000);//每2秒判断一次设备是否在线
+                }
+                SaveVideo(sn, RTMPhd);
+                isOnline = false;
+            }
+            /*
+            SaveVideo(sn, RTMPhd);
+            Timer timer = new Timer(20 * 60 * 1000); //10分钟存储一次
+            timer.Enabled = true;
+            timer.Elapsed += (sender, e) => SaveVideo(sn, RTMPhd);*/
+        }
+        //private static void SaveVideo(object sender, ElapsedEventArgs e)
+        private static bool SaveVideo(string sn, string RTMPhd)
+        {
+            string baseDir = @"D:\ffmpeg\";
+            //string baseDir = @"K:\1.1\ffmpeg-20170731-b664d1f-win64-static\bin\";
+            if (!Directory.Exists(baseDir + sn))
+            {
+                Directory.CreateDirectory(baseDir + sn);
+            }
+            DateTime Now = DateTime.Now;
+            string date = DateTime.Now.ToString("yyMMddHHmm");
+            using (SmartBuildingEntities sme = new SmartBuildingEntities()) {
+                VideoPath vp = new VideoPath()
+                {
+                    VideoName = date + ".mp4",
+                    StartTime = Now,
+                    SN = sn
+                };
+                sme.VideoPath.Add(vp);
+                sme.SaveChanges();
+            }
+
+            string spath = baseDir;
+            string topath = baseDir + sn + @"\";
+            RTMPhd = RTMPhd.Replace("\r\n", "");
+            string cmd = @"D: && cd " + spath + " &&  ffmpeg -y -i "+RTMPhd+" -t 1200 -vcodec copy -acodec copy -f mp4 " + topath + date + ".mp4";
+            Console.WriteLine("****************S*****************");
+            Console.WriteLine(cmd);
+            Console.WriteLine("****************E*****************");
+            //string cmd = @"-y -i rtmp://live.hkstv.hk.lxdns.com/live/hks -vcodec copy -acodec copy -f mp4 K:\Project\姚门项目\智慧工地\管理系统\demo1\Mag\video\"+date+".mp4";
+            EXECMD(cmd);
+            return true; //表示执行完了
+        }
+        private static void EXECMD(string cmd)//object ocmd)
+        {
+            //string cmd = ocmd.ToString();
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = false;//由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = false;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            p.Start();//启动程序
+            //向cmd窗口发送输入信息
+            p.StandardInput.WriteLine(cmd + "&exit");
+            //p.StandardInput.WriteLine(cmd);
+            p.StandardInput.AutoFlush = true;
+            //p.StandardInput.WriteLine("exit");
+            //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
+            //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
+            //获取cmd窗口的输出信息
+            //string output = p.StandardOutput.ReadToEnd();
+            //StreamReader reader = p.StandardOutput;
+            //string line = reader.ReadLine();
+            //while (!reader.EndOfStream)
+            //{
+
+            //    line = reader.ReadLine();
+            //}             
+            p.WaitForExit();//等待程序执行完退出进程
+            p.Close();
+            // return output;
+        }
+    }
+}
